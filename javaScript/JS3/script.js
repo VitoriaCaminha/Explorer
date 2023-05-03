4a7610f480f4ddde4ca1aa26d4f2efaba0d810f1
VitoriaCaminha <vitoria@praciano.com.br>
2023-05-03 09:39:24 -0300

    JavaScript concepts

--- javaScript/JS3/script.js
+++ javaScript/JS3/script.js
@@ -41,13 +41,86 @@
 
 // alert('Os nomes digitados foram: ' + items)
 
-let result = prompt('Adivinhe um número de 0 a 10')
-const randomNumber = Math.round(Math.random() * 10)
-let attempts = 1
+// let result = prompt('Adivinhe um número de 0 a 10')
+// const randomNumber = Math.round(Math.random() * 10)
+// let attempts = 1
 
-while (Number(result) != randomNumber) {
-  result = prompt('Você errou, tente novamente')
-  attempts++
-}
+// while (Number(result) != randomNumber) {
+//   result = prompt('Você errou, tente novamente')
+//   attempts++
+// }
 
-alert('Parabéns, você acertou o número em ' + attempts + ' tentativas')
+// alert('Parabéns, você acertou o número em ' + attempts + ' tentativas')
+
+// let userNumber
+// let items = []
+
+// while (userNumber != 3) {
+//   let userNumber = Number(
+//     prompt(`Olá! Digite o número da opção desejada:
+//             1. Cadastrar um item na lista;
+//             2. Mostrar itens cadastrados;
+//             3. Sair do programa`)
+//   )
+
+//   if (userNumber == 1) {
+//     let item = prompt('Digite o item que você quer cadastrar')
+//     items.push(item)
+//   } else if (userNumber == 2) {
+//     if (items.length == 0) {
+//       alert('Não existem itens cadastrados')
+//     } else {
+//       alert(items)
+//     }
+//   } else {
+//     alert('Até mais!')
+//   }
+// }
+
+// const patients = [
+//   {
+//     name: 'Vitória',
+//     age: 24,
+//     weight: 78,
+//     height: 178
+//   },
+//   {
+//     name: 'Luiz',
+//     age: 20,
+//     weight: 85,
+//     height: 180
+//   },
+//   {
+//     name: 'Fernando',
+//     age: 29,
+//     weight: 70,
+//     height: 160
+//   }
+// ]
+
+// // let patientsNames = []
+
+// // for (let i; i < patients.length; i++) {
+// //   patientsNames[i] = patients[i].name
+// // }
+
+// // for (let patient of patients) {
+// //   patientsNames.push(patient.name)
+// // }
+
+// // alert(patientsNames)
+
+// function IMC (weight, height) {
+//   return (weight / (height / 100) ** 2).toFixed(2)
+// }
+
+// function patientsIMC (patient) {
+//   return `
+//   Paciente ${patient.name} possui IMC de ${IMC(patient.weight, patient.height)}
+//   `
+// }
+
+// for (let patient of patients) {
+//   let IMCmessage = patientsIMC(patient)
+//   alert(IMCmessage)
+// }
