7f5284717036cfa374336de8bc1a2a69c5466e00
VitoriaCaminha <vitoria@praciano.com.br>
2023-05-18 20:01:46 -0300

    javascript

--- javaScript/JS4/script.js
+++ javaScript/JS4/script.js
@@ -1,15 +1,32 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 const randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 
-function handleClick (event) {
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+// função callback
+function handleTryClick (event) {
   event.preventDefault()
   const inputNumber = document.querySelector('#inputNumber')
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector('.screen1').classList.add("hide")
-    document.querySelector('.screen2').classList.remove("hide")
-    document.querySelector('.screen2 h2').innerText = 
-    `Acertou em ${attempts} tentativas`
+    screen1.classList.add('hide')
+    screen2.classList.remove('hide')
+    screen2.querySelector('h2').innerText = `Acertou em ${attempts} tentativas`
   }
 
+  inputNumber.value = ''
   attempts++
 }
+
+function handleResetClick (event) {
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+  attempts = 1
+}
+
+// eventos
+
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
